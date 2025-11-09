f609ab092d972af9a304f4537971578f531533c4
ZZAMBAs <rksidksrksi@naver.com>
2025-11-09 15:43:47 +0900

    refactor(contract): 계약 생성 응답 DTO 수정

--- contract-service/src/main/java/com/example/contractservice/contract/controller/dto/response/ContractCreateResponse.java
+++ contract-service/src/main/java/com/example/contractservice/contract/controller/dto/response/ContractCreateResponse.java
@@ -1,14 +1,7 @@
 package com.example.contractservice.contract.controller.dto.response;
 
-import java.time.Instant;
-
 public record ContractCreateResponse(
-    String code,
-    Instant startedAt,
-    Instant endedAt,
-    long unitAmount,
-    String name,
-    String body
+    String code
 ) {
 
 }
