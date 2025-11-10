b33e1faccd897feeb90cc718d4ed386f6b7cf132
ZZAMBAs <rksidksrksi@naver.com>
2025-11-10 16:39:19 +0900

    feat(contract): 계약 생성 예외 추가

---
+++ contract-service/src/main/java/com/example/contractservice/contract/domain/exception/ContractCreateException.java
@@ -0,0 +1,14 @@
+package com.example.contractservice.contract.domain.exception;
+
+public class ContractCreateException extends RuntimeException {
+    private final CreateErrorCode errorCode;
+
+    public ContractCreateException(CreateErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public ContractCreateException(Throwable cause, CreateErrorCode errorCode) {
+        super(cause);
+        this.errorCode = errorCode;
+    }
+}
---
+++ contract-service/src/main/java/com/example/contractservice/contract/domain/exception/CreateErrorCode.java
@@ -0,0 +1,15 @@
+package com.example.contractservice.contract.domain.exception;
+
+public enum CreateErrorCode {
+    NO_FREELANCERS(100, "프리랜서가 없어 계약을 생성할 수 없습니다."),
+    INVALID_MEMBER(101, "존재하지 않는 회원으로 계약을 생성할 수 없습니다."),
+    DELETED_MEMBER(102, "탈퇴한 회원은 계약을 생성할 수 없습니다.");
+
+    private int statusCode;
+    private String message;
+
+    CreateErrorCode(int statusCode, String message) {
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+}
