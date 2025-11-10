de7d71d8852a8963bbc7f92e7f79ade4ec8a5c93
haeun9988 <haeun9988@naver.com>
2025-11-10 17:17:01 +0900

    feat(profile): 의존성 추가 및 ProfileServiceApplication 어노테이션 추가

--- profile-service/src/main/java/com/example/profileservice/ProfileServiceApplication.java
+++ profile-service/src/main/java/com/example/profileservice/ProfileServiceApplication.java
@@ -2,12 +2,16 @@ package com.example.profileservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProfileServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProfileServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileServiceApplication.class, args);
+    }
 
 }
