b618d882541c61110ce7f1185116faa7b6c0ef2a
ZZAMBAs <rksidksrksi@naver.com>
2025-11-10 10:01:40 +0900

    feat(contract): 카프카 설정 추가

---
+++ contract-service/src/main/java/com/example/contractservice/config/KafkaTemplateConfig.java
@@ -0,0 +1,26 @@
+package com.example.contractservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+@Configuration
+public class KafkaTemplateConfig {
+
+    @Value("${kafka.config.topic-partitions}")
+    private int topicPartitions;
+
+    @Value("${kafka.config.topic-replications}")
+    private int topicReplications;
+
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate(
+        ProducerFactory<String, Object> producerFactory
+    ) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+}
