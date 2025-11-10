f3bda28095c2658546d9bd41db50cb5a945d6f83
ZZAMBAs <rksidksrksi@naver.com>
2025-11-10 15:02:18 +0900

    docs(contract): 계약 생성 API 명세 수정

--- contract-service/src/main/java/com/example/contractservice/contract/common/swagger/annotation/ContractCreateApi.java
+++ contract-service/src/main/java/com/example/contractservice/contract/common/swagger/annotation/ContractCreateApi.java
@@ -1,6 +1,9 @@
 package com.example.contractservice.contract.common.swagger.annotation;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -9,6 +12,9 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Operation(summary = "계약 생성", description = "REQUESTED 상태인 계약을 생성")
+@Parameters({
+        @Parameter(name = "X-CODE", description = "로그인 사용자 코드", in = ParameterIn.HEADER, required = true)
+})
 public @interface ContractCreateApi {
 
 }
