bf2f84e9358f6ce10484da67400c64f75ccb790d
ZZAMBAs <rksidksrksi@naver.com>
2025-11-11 22:58:52 +0900

    feat(contract): Deposit Repository 로직 추가

--- contract-service/src/main/java/com/example/contractservice/deposit/repository/DepositJpaRepository.java
+++ contract-service/src/main/java/com/example/contractservice/deposit/repository/DepositJpaRepository.java
@@ -1,8 +1,10 @@
 package com.example.contractservice.deposit.repository;
 
 import com.example.contractservice.deposit.entity.DepositEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DepositJpaRepository extends JpaRepository<DepositEntity, Long> {
 
+    Optional<DepositEntity> findByMemberCode(String memberCode);
 }
