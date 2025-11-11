e91f8ef26461efc2c93c143812b87e1e0d395945
programofktw <thkim000317@naver.com>
2025-11-11 16:51:31 +0900

    feat(member): kakao social login 비즈앱 등록 이후 email 정보 수집

--- member-service/src/main/java/com/example/memberservice/common/security/model/vo/KakaoUserInfo.java
+++ member-service/src/main/java/com/example/memberservice/common/security/model/vo/KakaoUserInfo.java
@@ -23,7 +23,7 @@ public class KakaoUserInfo implements OAuthUserInfo {
     @Override
     public String getEmail() {
         Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
-        String email = null;//= kakaoAccount.get("email").toString();
+        String email = kakaoAccount.get("email").toString();
 
         //카카오는 이메일을 받아오기 위해 프론트 화면 생성 이후 검수를 받아야함.
         if (email == null) {
