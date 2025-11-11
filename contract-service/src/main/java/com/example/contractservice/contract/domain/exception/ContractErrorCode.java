a31a4439dc0400c9ab263f9556fe5bd37ad49924
ZZAMBAs <rksidksrksi@naver.com>
2025-11-11 09:43:44 +0900

    refactor(contract): 계약 예외 통합

--- contract-service/src/main/java/com/example/contractservice/contract/domain/exception/ContractErrorCode.java
+++ contract-service/src/main/java/com/example/contractservice/contract/domain/exception/ContractErrorCode.java
@@ -1,6 +1,6 @@
 package com.example.contractservice.contract.domain.exception;
 
-public enum CreateErrorCode {
+public enum ContractErrorCode {
     NO_FREELANCERS(100, "프리랜서가 없어 계약을 생성할 수 없습니다."),
     INVALID_MEMBER(101, "존재하지 않는 회원으로 계약을 생성할 수 없습니다."),
     DELETED_MEMBER(102, "탈퇴한 회원은 계약을 생성할 수 없습니다.");
@@ -8,7 +8,7 @@ public enum CreateErrorCode {
     private final int statusCode;
     private final String message;
 
-    CreateErrorCode(int statusCode, String message) {
+    ContractErrorCode(int statusCode, String message) {
         this.statusCode = statusCode;
         this.message = message;
     }
