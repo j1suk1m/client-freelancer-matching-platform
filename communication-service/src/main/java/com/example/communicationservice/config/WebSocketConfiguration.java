6b65f335dda1dce699464052ed3663edd1e6d657
jisukimtech <jisukim.tech@gmail.com>
2025-11-12 15:43:57 +0900

    feat(config): WebSocket STOMP 기본 설정 추가
    - `@EnableWebSocketMessageBroker` 적용
    - 메시지 브로커 prefix 및 endpoint 설정 (/app, /queue)

---
+++ communication-service/src/main/java/com/example/communicationservice/config/WebSocketConfiguration.java
@@ -0,0 +1,26 @@
+package com.example.communicationservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/queue"); // 메시지 브로커의 구독 prefix
+        registry.setApplicationDestinationPrefixes("/app"); // @MessageMapping이 붙은 컨트롤러로 라우팅할 prefix
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat") // STOMP 클라이언트가 최초로 WebSocket handshake를 요청하는 URL
+            .setAllowedOrigins("http://localhost:8080", "http://localhost:3000") // CORS 허용
+            .withSockJS();
+    }
+
+}
