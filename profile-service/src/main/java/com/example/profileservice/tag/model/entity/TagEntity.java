c1818febd88e7e9b8cd6a84b93a1dadbdee6e166
haeun9988 <haeun9988@naver.com>
2025-11-12 15:08:52 +0900

    fix: entity 수정

--- profile-service/src/main/java/com/example/profileservice/tag/model/entity/TagEntity.java
+++ profile-service/src/main/java/com/example/profileservice/tag/model/entity/TagEntity.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
+import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -27,11 +28,12 @@ public class TagEntity {
     private String skill;
 
     // 기술 코드
-    @Column(length = 36, nullable = false, unique = true)
+    @Column(length = 36, nullable = false, unique = true, updatable = false)
     private String code;
 
     @Builder
     public TagEntity(String skill) {
+        this.code = UUID.randomUUID().toString();
         this.skill = skill;
     }
 }
