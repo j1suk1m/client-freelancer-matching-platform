2177d019d677092853b7e23b1b87e9f7fa64f114
programofktw <thkim000317@naver.com>
2025-11-12 18:34:46 +0900

    feat(auth): errorCode 파라미터 추가

--- member-service/src/main/java/com/example/memberservice/common/exception/ErrorCode.java
+++ member-service/src/main/java/com/example/memberservice/common/exception/ErrorCode.java
@@ -3,21 +3,22 @@ package com.example.memberservice.common.exception;
 public enum ErrorCode {
 
     //401
-    UNAUTHORIZATION("인증되지 않은 요청입니다.", 401),
+    UNAUTHORIZATION(401, "인증되지 않은 요청입니다.", "UNAUTHORIZATION"),
 
     //404
-    MEMBER_NOT_FOUND("요청하신 사용자를 찾을 수 없습니다.",404),
-
+    MEMBER_NOT_FOUND(404, "요청하신 사용자를 찾을 수 없습니다.", "MEMBER_NOT_FOUND"),
 
     //500
-    DATA_SAVE_FAILED("데이터 저장에 실패했습니다.", 500);
+    DATA_SAVE_FAILED(500, "데이터 저장에 실패했습니다.", "DATA_SAVE_FAILED");
 
     private final String message;
     private final int statusCode;
+    private final String errorCode;
 
-    ErrorCode(String message, int statusCode) {
+    ErrorCode(int statusCode, String message, String errorCode) {
         this.message = message;
         this.statusCode = statusCode;
+        this.errorCode = errorCode;
     }
 
     public String getMessage() {
@@ -26,5 +27,9 @@ public enum ErrorCode {
 
     public int getStatusCode() {
         return statusCode;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
     }
 }
