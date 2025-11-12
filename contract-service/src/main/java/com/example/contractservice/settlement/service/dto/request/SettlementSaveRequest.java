5932912609133783ca88274ef7b171e26f9e3031
ZZAMBAs <rksidksrksi@naver.com>
2025-11-12 09:55:00 +0900

    fix(contract): 도메인 변환 로직 삭제

--- contract-service/src/main/java/com/example/contractservice/settlement/service/dto/request/SettlementSaveRequest.java
+++ contract-service/src/main/java/com/example/contractservice/settlement/service/dto/request/SettlementSaveRequest.java
@@ -1,9 +1,6 @@
 package com.example.contractservice.settlement.service.dto.request;
 
 import com.example.contractservice.common.PaymentType;
-import com.example.contractservice.settlement.domain.Settlement;
-import com.example.contractservice.settlement.domain.vo.SettlementReference;
-import com.example.contractservice.settlement.domain.vo.SettlementStatusInfo;
 import java.time.Instant;
 
 public record SettlementSaveRequest(
@@ -15,10 +12,4 @@ public record SettlementSaveRequest(
         PaymentType paymentType
 ) {
 
-    public Settlement toSettlement() {
-        return new Settlement(
-                new SettlementReference(receiverCode, contractCode),
-                new SettlementStatusInfo(amount, )
-        )
-    }
 }
