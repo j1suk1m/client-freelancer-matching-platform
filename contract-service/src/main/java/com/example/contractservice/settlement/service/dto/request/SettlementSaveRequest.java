79b3dcbf0631805baa53a0bbd0576b0e8dba930b
ZZAMBAs <rksidksrksi@naver.com>
2025-11-12 09:51:29 +0900

    feat(contract): 도메인으로 변환 로직 추가

--- contract-service/src/main/java/com/example/contractservice/settlement/service/dto/request/SettlementSaveRequest.java
+++ contract-service/src/main/java/com/example/contractservice/settlement/service/dto/request/SettlementSaveRequest.java
@@ -1,6 +1,9 @@
 package com.example.contractservice.settlement.service.dto.request;
 
 import com.example.contractservice.common.PaymentType;
+import com.example.contractservice.settlement.domain.Settlement;
+import com.example.contractservice.settlement.domain.vo.SettlementReference;
+import com.example.contractservice.settlement.domain.vo.SettlementStatusInfo;
 import java.time.Instant;
 
 public record SettlementSaveRequest(
@@ -12,4 +15,10 @@ public record SettlementSaveRequest(
         PaymentType paymentType
 ) {
 
+    public Settlement toSettlement() {
+        return new Settlement(
+                new SettlementReference(receiverCode, contractCode),
+                new SettlementStatusInfo(amount, )
+        )
+    }
 }
