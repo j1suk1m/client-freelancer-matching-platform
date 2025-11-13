1d100f3a2aabcbeb715ddbc22a0eda3490289e49
ZZAMBAs <rksidksrksi@naver.com>
2025-11-13 15:07:51 +0900

    feat(contract): updatedAt 변경 추적을 위해 @LastModifiedDate 추가

--- contract-service/src/main/java/com/example/contractservice/common/entity/BaseEntity.java
+++ contract-service/src/main/java/com/example/contractservice/common/entity/BaseEntity.java
@@ -11,6 +11,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
@@ -27,7 +28,7 @@ public class BaseEntity {
     @Column(name = "created_at", nullable = false)
     private Instant createdAt; // BaseEntity
 
-    @CreatedDate
+    @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
     private Instant updatedAt; // BaseEntity
 
