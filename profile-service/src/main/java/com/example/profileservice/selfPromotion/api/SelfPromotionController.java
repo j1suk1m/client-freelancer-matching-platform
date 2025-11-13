fa9ee01c420668dfbd1e75388946ad5ce27c59a2
haeun9988 <haeun9988@naver.com>
2025-11-13 17:11:38 +0900

    fix: controller, apiController 수정

--- profile-service/src/main/java/com/example/profileservice/selfPromotion/api/SelfPromotionController.java
+++ profile-service/src/main/java/com/example/profileservice/selfPromotion/api/SelfPromotionController.java
@@ -12,9 +12,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -51,12 +51,13 @@ public class SelfPromotionController implements SelfPromotionApiController {
         return ResponseEntity.ok(ResponseDto.success(promotions));
     }
 
-    // 프로모션 등록 (인증 필요)
+    // 프로모션 등록
     @Override
     @PostMapping
     public ResponseEntity<ResponseDto<SelfPromotionResponse>> createPromotion(
             @RequestHeader(value = "X-CODE", defaultValue = DEFAULT_MEMBER_CODE) String memberCode,
             @Valid @RequestBody SelfPromotionCreateRequest request) {
+
         SelfPromotionResponse newPromotion = selfPromotionService.createPromotion(memberCode, request);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(ResponseDto.success(newPromotion));
@@ -72,9 +73,9 @@ public class SelfPromotionController implements SelfPromotionApiController {
         return ResponseEntity.ok(ResponseDto.success(promotion));
     }
 
-    // 프로모션 수정 (인증 및 권한 필요)
+    // 프로모션 수정
     @Override
-    @PutMapping("/{promotionCode}") // PATCH 대신 전체 수정을 의미하는 PUT을 사용했습니다.
+    @PatchMapping("/{promotionCode}")
     public ResponseEntity<ResponseDto<SelfPromotionResponse>> updatePromotion(
             @RequestHeader(value = "X-CODE", defaultValue = DEFAULT_MEMBER_CODE) String memberCode,
             @PathVariable String promotionCode,
@@ -84,7 +85,7 @@ public class SelfPromotionController implements SelfPromotionApiController {
         return ResponseEntity.ok(ResponseDto.success(updatedPromotion));
     }
 
-    // 프로모션
+    // 프로모션 삭제
     @Override
     @DeleteMapping("/{promotionCode}")
     public ResponseEntity<ResponseDto<Void>> deletePromotion(
