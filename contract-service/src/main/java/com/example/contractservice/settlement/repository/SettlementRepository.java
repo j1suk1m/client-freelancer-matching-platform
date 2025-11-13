1bf92534fc07edcd2b786c3eaf97fa3715550851
ZZAMBAs <rksidksrksi@naver.com>
2025-11-13 15:55:16 +0900

    feat(contract): 정산 데이터 저장 로직 추가

--- contract-service/src/main/java/com/example/contractservice/settlement/repository/SettlementRepository.java
+++ contract-service/src/main/java/com/example/contractservice/settlement/repository/SettlementRepository.java
@@ -1,6 +1,7 @@
 package com.example.contractservice.settlement.repository;
 
 import com.example.contractservice.settlement.entity.SettlementEntity;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +12,9 @@ public class SettlementRepository {
 
     public SettlementEntity save(SettlementEntity settlementEntity) {
         return settlementJpaRepository.save(settlementEntity);
+    }
+
+    public void saveAll(List<SettlementEntity> settlementEntities) {
+        settlementJpaRepository.saveAll(settlementEntities);
     }
 }
