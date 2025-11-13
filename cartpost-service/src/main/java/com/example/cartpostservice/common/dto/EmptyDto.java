c292d496c85ff1e00be3e47ad66586b6c1f59a9c
sora0319 <sojeoung536985@gmail.com>
2025-11-13 22:54:19 +0900

    chore(cart,post): code convention에 맞추어 수정

--- cartpost-service/src/main/java/com/example/cartpostservice/common/dto/EmptyDto.java
+++ cartpost-service/src/main/java/com/example/cartpostservice/common/dto/EmptyDto.java
@@ -1,9 +1,11 @@
 package com.example.cartpostservice.common.dto;
 
 public final class EmptyDto {
+
     private static final EmptyDto INSTANCE = new EmptyDto();
 
-    private EmptyDto() {}
+    private EmptyDto() {
+    }
 
     public static EmptyDto getInstance() {
         return INSTANCE;
