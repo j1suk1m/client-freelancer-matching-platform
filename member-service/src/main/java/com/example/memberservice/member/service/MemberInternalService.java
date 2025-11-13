56a7226b077d7a4fe3703b2175e528559a449025
programofktw <thkim000317@naver.com>
2025-11-13 14:32:36 +0900

    feat(member): Internal 유저 정보 조회 Service 메서드 구현

--- member-service/src/main/java/com/example/memberservice/member/service/MemberInternalService.java
+++ member-service/src/main/java/com/example/memberservice/member/service/MemberInternalService.java
@@ -4,8 +4,12 @@ import com.example.memberservice.member.entity.Members;
 import com.example.memberservice.member.repository.MemberJpaRepository;
 import com.example.memberservice.member.service.model.dto.output.MemberExistOutput;
 import com.example.memberservice.member.service.model.dto.output.MemberInfoOutput;
+import com.example.memberservice.member.service.model.vo.MemberInfo;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,20 +21,35 @@ public class MemberInternalService {
 
     private final MemberJpaRepository memberJpaRepository;
 
-    public List<MemberInfoOutput> getMemberInfos(List<String> memberCodes){
+    public List<MemberInfoOutput> getMemberInfos(List<String> memberCodes) {
         List<Members> findMembers = memberJpaRepository.findAllByCode(memberCodes);
 
-        return null;
+        Set<String> setMemberCodes = new HashSet<>(memberCodes);
+
+        if (setMemberCodes.size() > findMembers.size()) {
+            //TODO BusinnessException 추가 이후
+            throw new IllegalArgumentException("요청하신 memberCode 중 없는 memberCode가 존재합니다.");
+        }
+
+        return List.of(
+            new MemberInfoOutput(
+                findMembers.stream()
+                    .map(m -> new MemberInfo(
+                        m.getCode(),
+                        m.getName(),
+                        m.getCanWork()
+                    ))
+                    .toList()
+            )
+        );
     }
 
-    public List<MemberExistOutput> getMemberExists(List<String> memberCodes){
+    public List<MemberExistOutput> getMemberExists(List<String> memberCodes) {
         List<Members> findMembers = memberJpaRepository.findAllByCode(memberCodes);
 
         List<String> exists = new ArrayList<>();
 
         List<String> notExists = new ArrayList<>();
-
-
 
         return null;
     }
