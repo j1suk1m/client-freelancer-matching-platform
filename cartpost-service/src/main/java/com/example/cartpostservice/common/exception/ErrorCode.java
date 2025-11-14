4f36fc116ab830fc551b2d1a170acff9db66afe4
sora0319 <sojeoung536985@gmail.com>
2025-11-15 02:44:31 +0900

    chore(cart, post): 사용 안하는 import 제거

--- cartpost-service/src/main/java/com/example/cartpostservice/common/exception/ErrorCode.java
+++ cartpost-service/src/main/java/com/example/cartpostservice/common/exception/ErrorCode.java
@@ -3,7 +3,6 @@ package com.example.cartpostservice.common.exception;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 
 @Getter
 @RequiredArgsConstructor
