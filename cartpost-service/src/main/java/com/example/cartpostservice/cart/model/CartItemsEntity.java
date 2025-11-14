aabf0beb2c984f40e75ad9b4711009553dda6a9b
sora0319 <sojeoung536985@gmail.com>
2025-11-15 02:48:01 +0900

    feat(cart): cart item entity 추가

---
+++ cartpost-service/src/main/java/com/example/cartpostservice/cart/model/CartItemsEntity.java
@@ -0,0 +1,29 @@
+package com.example.cartpostservice.cart.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "cart_itmes")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CartItemsEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(name = "contract_code", nullable = false, updatable = false)
+    private String contractCode;
+
+    @Column(name = "status", nullable = false, updatable = false)
+    private String status;
+}
