d3430656d27fe60a268d3081723d52fecc8826d4
sora0319 <sojeoung536985@gmail.com>
2025-11-14 15:14:26 +0900

    feat(cart,post): swagger schema 추가

--- cartpost-service/src/main/java/com/example/cartpostservice/common/exception/FieldErrorDetail.java
+++ cartpost-service/src/main/java/com/example/cartpostservice/common/exception/FieldErrorDetail.java
@@ -1,16 +1,20 @@
 package com.example.cartpostservice.common.exception;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import java.util.stream.Collectors;
-import lombok.Builder;
-import lombok.RequiredArgsConstructor;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 
 public class FieldErrorDetail {
 
+    @Schema(description = "에러가 발생한 필드명", example = "email")
     private String field;
+
+    @Schema(description = "에러가 발생한 이유 (검증 메시지)", example = "이메일 형식이 올바르지 않습니다.")
     private String reason;
+
+    @Schema(description = "제출된 잘못된 값", example = "testUser@")
     private String value;
 
     private FieldErrorDetail(FieldError error) {
