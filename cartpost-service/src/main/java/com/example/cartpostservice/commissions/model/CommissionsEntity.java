784ce2d8e1c5e9047deca48f0ee109ddf945ad70
sora0319 <sojeoung536985@gmail.com>
2025-11-15 04:47:37 +0900

    feat(post): 의뢰글 엔티티 생성

---
+++ cartpost-service/src/main/java/com/example/cartpostservice/commissions/model/CommissionsEntity.java
@@ -0,0 +1,32 @@
+package com.example.cartpostservice.commissions.model;
+
+import com.example.cartpostservice.commissions.model.vo.PaymentType;
+import com.example.cartpostservice.common.model.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Table(name = "commissions")
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommissionsEntity extends BaseEntity {
+
+    private String memberCode;
+
+    private String title;
+
+    private String content;
+
+    private PaymentType paymentType;
+
+    private String unitAmount;
+
+    private LocalDate startedAt;
+
+    private LocalDate endedAt;
+
+    private boolean isOpen;
+
+}
