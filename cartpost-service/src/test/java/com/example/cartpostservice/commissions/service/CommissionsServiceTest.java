3598c0fb4580570abc159839a783e0c0bb958458
sora0319 <sojeoung536985@gmail.com>
2025-11-15 17:00:07 +0900

    test(post): 의뢰글 service 테스트 기본 틀 생성

---
+++ cartpost-service/src/test/java/com/example/cartpostservice/commissions/service/CommissionsServiceTest.java
@@ -0,0 +1,77 @@
+package com.example.cartpostservice.commissions.service;
+
+import com.example.cartpostservice.commissions.model.CommissionsEntity;
+import com.example.cartpostservice.commissions.repository.CommissionsRepository;
+import com.example.cartpostservice.commissions.service.dto.response.CommissionCreateResult;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class) //Mockito + JUnit 연동
+class CommissionsServiceTest {
+
+    @Mock
+    private CommissionsRepository commissionsRepository;
+
+    @InjectMocks
+    private CommissionsServiceImpl commissionsService;
+
+    private String xCode;
+    private String commissionCode;
+
+    @BeforeEach
+    void setUp() {
+        xCode = UUID.randomUUID().toString();
+        commissionCode = UUID.randomUUID().toString();
+    }
+
+    @Test
+    void createCommission_success() {
+
+        /*
+        // given
+        when(commissionsRepository.save(any())).thenReturn(new CommissionsEntity());
+
+        // when
+        CommissionCreateResult result = commissionsService.createCommission(xCode);
+
+        // then
+        assertThat(result).isNotNull();
+        verify(commissionsRepository, times(1)).save(any());
+         */
+    }
+
+    @Test
+    void readCommission_success() {
+
+    }
+
+    @Test
+    void updateCommission_success() {
+
+    }
+
+    @Test
+    void deleteCommission_success() {
+
+    }
+
+    @Test
+    void finishCommission_success() {
+
+    }
+
+    @Test
+    void readOwnCommissions_success() {
+
+    }
+}
+
