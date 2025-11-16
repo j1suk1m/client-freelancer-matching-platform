a4bfb1660a2457b24411157d5acdd7e97a3172e8
programofktw <thkim000317@naver.com>
2025-11-17 08:52:26 +0900

    feat(member): Kafka 관련 의존성 추가

--- member-service/src/main/java/com/example/memberservice/member/service/MemberServiceImpl.java
+++ member-service/src/main/java/com/example/memberservice/member/service/MemberServiceImpl.java
@@ -7,10 +7,18 @@ import com.example.memberservice.member.service.model.dto.input.MemberExistByNam
 import com.example.memberservice.member.service.model.dto.input.MemberGetInput;
 import com.example.memberservice.member.service.model.dto.input.MemberUpdateInput;
 import com.example.memberservice.member.service.model.dto.input.MemberUpdateWorkStateInput;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
+
+    // 멤버 조회에서 태그 정보와 평가 정보를 받아올 RestTemplate
+    private final RestTemplate restTemplate;
 
     @Override
     public UserGetResponse getMemberByCode(MemberGetInput input) {
