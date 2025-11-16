82a090f44e65950ed6804b995e07701836b2fe69
sora0319 <sojeoung536985@gmail.com>
2025-11-17 07:06:55 +0900

    feat(cart): 사용자아게 보여주기 위한 데이터 추가

--- cartpost-service/src/main/java/com/example/cartpostservice/cart/model/CartItemsEntity.java
+++ cartpost-service/src/main/java/com/example/cartpostservice/cart/model/CartItemsEntity.java
@@ -1,25 +1,34 @@
 package com.example.cartpostservice.cart.model;
 
+import com.example.cartpostservice.common.model.vo.PaymentType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.time.Instant;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Table(name = "cart_items")
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
 @Entity
+@Table(name = "cart_items")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 public class CartItemsEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+    private Long id; // 필드명 소문자로 수정 권장
 
     @Column(name = "contract_code", nullable = false, updatable = false)
     private String contractCode;
@@ -27,6 +36,20 @@ public class CartItemsEntity {
     @Column(name = "cart_code", nullable = false, updatable = false)
     private String cartCode;
 
-    @Column(name = "status", nullable = false, updatable = false)
+    @Column(name = "status", nullable = false)
     private String status;
+
+    @Column(name = "started_at", nullable = false)
+    private Instant startedAt;
+
+    @Column(name = "ended_at", nullable = false)
+    private Instant endedAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "payment_type", nullable = false)
+    private PaymentType paymentType;
+
+    @Column(name = "amount", nullable = false)
+    private String amount;
 }
+
