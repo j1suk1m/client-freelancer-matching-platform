745a35148bde52eb965c43e4b082341bbea9a46d
sora0319 <sojeoung536985@gmail.com>
2025-11-17 00:42:22 +0900

    feat(payment): payment, order 테이블 생성 DDL 추가

--- payment-service/src/main/resources/schema.sql
+++ payment-service/src/main/resources/schema.sql
@@ -1,13 +1,22 @@
-CREATE TABLE IF NOT EXISTS `payments` (
-      `id` BIGINT NOT NULL,
-      `member_code` CHAR(36) NOT NULL,
-      `code` CHAR(36) NOT NULL,
-      `created_at` TIMESTAMP NOT NULL,
-      `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-      `is_deleted` BOOLEAN NOT NULL,
-      `total_amount` BIGINT NOT NULL,
-      `payment_key` VARCHAR(255) NOT NULL,
-      `pg_order_id` VARCHAR(255) NOT NULL,
-      `status` VARCHAR(20) NOT NULL,
-      PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS orders (
+                              id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                              code VARCHAR(255) NOT NULL UNIQUE,
+                              is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
+                              created_at TIMESTAMP,
+                              updated_at TIMESTAMP,
+                              member_code VARCHAR(255) NOT NULL,
+                              order_pg_id VARCHAR(255) NOT NULL UNIQUE
 );
+
+CREATE TABLE IF NOT EXISTS payments (
+                          id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                          code VARCHAR(255) NOT NULL UNIQUE,
+                          is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
+                          created_at TIMESTAMP,
+                          updated_at TIMESTAMP,
+                          order_pg_id VARCHAR(255) NOT NULL UNIQUE,
+                          payment_key VARCHAR(255),
+                          amount BIGINT NOT NULL,
+                          payment_status VARCHAR(255) NOT NULL
+);
+
