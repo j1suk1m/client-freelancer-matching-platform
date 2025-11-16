10d4f9e7cb40e86187a3a12145d8f12d159e19fe
sora0319 <sojeoung536985@gmail.com>
2025-11-17 03:00:41 +0900

    fix(post): ID가 Long 기준으로 되어 있는데 code를 기준으로 String으로 등록된 것을 수정

--- cartpost-service/src/main/java/com/example/cartpostservice/commissions/repository/CommissionsTagRepository.java
+++ cartpost-service/src/main/java/com/example/cartpostservice/commissions/repository/CommissionsTagRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CommissionsTagRepository extends JpaRepository<CommissionsTagEntity, String> {
+public interface CommissionsTagRepository extends JpaRepository<CommissionsTagEntity, Long> {
 
 }
