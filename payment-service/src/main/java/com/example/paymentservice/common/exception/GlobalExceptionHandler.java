ef41794a12b5b503a1cd4b557db9aeb866ad6f94
sora0319 <sojeoung536985@gmail.com>
2025-11-17 00:38:24 +0900

    feat(payment): 전체 예외에 관리 handler 생성

---
+++ payment-service/src/main/java/com/example/paymentservice/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,50 @@
+package com.example.paymentservice.common.exception;
+
+import com.example.paymentservice.common.dto.EmptyResponse;
+import com.example.paymentservice.common.dto.ResponseDto;
+import com.example.paymentservice.common.dto.enums.CustomStatusCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    protected ResponseEntity<ResponseDto<EmptyResponse>> handleBusinessException(BusinessException ex) {
+        log.warn("handleBusinessException: {}", ex.getMessage());
+
+        CustomStatusCode customStatusCode = ex.getCustomStatusCode();
+        ResponseDto response = ResponseDto.of(customStatusCode);
+
+        return new ResponseEntity<>(response, customStatusCode.getStatus());
+    }
+
+
+    //    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    protected ResponseEntity<ResponseDto<List<FieldErrorDetail>>> handleMethodArgumentNotValidException(
+//            MethodArgumentNotValidException ex) {
+//        log.warn("handleMethodArgumentNotValidException: {}", ex.getMessage());
+//
+//        BindingResult bindingResult = ex.getBindingResult();
+//        ErrorCode errorCode = ErrorCode.INVALID_INPUT_VALUE;
+//
+//        // ResponseDto의 오버로딩된 of() 사용
+//        ResponseDto<List<FieldErrorDetail>> response = ResponseDto.of(errorCode, bindingResult);
+//
+//        return new ResponseEntity<>(response, errorCode.getStatus());
+//    }
+//
+//
+//    @ExceptionHandler(Exception.class)
+//    protected ResponseEntity<ResponseDto<EmptyDto>> handleGeneralException(Exception ex) {
+//        log.error("handleGeneralException: {}", ex.getMessage(), ex); // 스택 트레이스 로깅
+//
+//        ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+//        ResponseDto response = ResponseDto.of(errorCode);
+//
+//        return new ResponseEntity<>(response, errorCode.getStatus());
+//    }
+}
