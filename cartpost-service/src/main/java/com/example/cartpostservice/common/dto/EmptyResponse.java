4add5431377c29e35eee538ea0d63304c03c5ccf
sora0319 <sojeoung536985@gmail.com>
2025-11-17 07:01:22 +0900

    feat(cart,post): 빈 데이터에 대해 직렬화를 위한 get 메소드 생성

--- cartpost-service/src/main/java/com/example/cartpostservice/common/dto/EmptyResponse.java
+++ cartpost-service/src/main/java/com/example/cartpostservice/common/dto/EmptyResponse.java
@@ -10,4 +10,8 @@ public final class EmptyResponse {
     public static EmptyResponse getInstance() {
         return INSTANCE;
     }
+
+    public String getEmpty() {
+        return ""; // 빈 필드 하나라도 있으면 직렬화 가능
+    }
 }
