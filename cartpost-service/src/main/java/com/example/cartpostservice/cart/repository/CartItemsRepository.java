cb52732c5a3efec539a3eabcc8bf601070bae6d4
sora0319 <sojeoung536985@gmail.com>
2025-11-17 07:04:48 +0900

    feat(cart): cart code로 cart item 목록 찾는 메서드 추가

--- cartpost-service/src/main/java/com/example/cartpostservice/cart/repository/CartItemsRepository.java
+++ cartpost-service/src/main/java/com/example/cartpostservice/cart/repository/CartItemsRepository.java
@@ -1,10 +1,12 @@
 package com.example.cartpostservice.cart.repository;
 
 import com.example.cartpostservice.cart.model.CartItemsEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CartItemsRepository extends JpaRepository<CartItemsEntity, String> {
 
+    List<CartItemsEntity> findByCartCode(String cartCode);
 }
