a7a06a45dd2035b5d8432182daff6ee231f6bc48
sora0319 <sojeoung536985@gmail.com>
2025-11-17 01:35:55 +0900

    feat(payment): 결제 목록, 결제 상세 보기 api 상세 문서 추가

--- payment-service/src/main/java/com/example/paymentservice/payment/controller/PaymentApi.java
+++ payment-service/src/main/java/com/example/paymentservice/payment/controller/PaymentApi.java
@@ -2,6 +2,8 @@ package com.example.paymentservice.payment.controller;
 
 import com.example.paymentservice.common.dto.ResponseDto;
 import com.example.paymentservice.payment.controller.dto.response.PayRechargeResponse;
+import com.example.paymentservice.payment.controller.dto.response.PaymentGetResponse;
+import com.example.paymentservice.payment.controller.dto.response.PaymentsGetResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -10,6 +12,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @Tag(name = "Payment API", description = "결제(충전) API 명세")
@@ -31,7 +34,30 @@ public interface PaymentApi {
     })
     ResponseEntity<ResponseDto<PayRechargeResponse>> payForRecharge(
             @RequestHeader(name = "X-CODE") String code,
-            String amount
+            @PathVariable String amount
     );
+
+    @Operation(summary = "결제 목록 조회", description = "모든 결제 목록을 조회합니다.")
+    @Parameters({
+            @Parameter(name = "X-CODE", description = "사용자 인증용 코드 (헤더)", required = true, in = ParameterIn.HEADER)
+    })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "결제 목록 조회 성공"),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류")
+    })
+    ResponseEntity<ResponseDto<PaymentsGetResponse>> getAllPayments(@RequestHeader(name = "X-CODE") String code);
+
+    @Operation(summary = "결제 상세 조회", description = "특정 주문 코드(orderId)를 기준으로 결제 상세 정보를 조회합니다.")
+    @Parameters({
+            @Parameter(name = "X-CODE", description = "사용자 인증용 코드 (헤더)", required = true, in = ParameterIn.HEADER),
+            @Parameter(name = "order-code", description = "조회할 주문 코드(orderId)", required = true, example = "123e4567-e89b-12d3-a456-426614174000", in = ParameterIn.PATH)
+    })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "결제 상세 조회 성공"),
+            @ApiResponse(responseCode = "404", description = "해당 주문 결제 정보 없음"),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류")
+    })
+    ResponseEntity<ResponseDto<PaymentGetResponse>> getPaymentByOrderCode(@RequestHeader(name = "X-CODE") String code,
+            @PathVariable("order-pg-id") String orderPgId);
 }
 
