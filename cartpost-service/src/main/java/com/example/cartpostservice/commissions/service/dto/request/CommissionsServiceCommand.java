5451f3893dbd9efd64989d003a8116660b992115
sora0319 <sojeoung536985@gmail.com>
2025-11-18 07:58:48 +0900

    chore(post): 공통 응답 구조 생성으로 제거

--- cartpost-service/src/main/java/com/example/cartpostservice/commissions/service/dto/request/CommissionsServiceCommand.java
+++ cartpost-service/src/main/java/com/example/cartpostservice/commissions/service/dto/request/CommissionsServiceCommand.java
@@ -1,12 +1,9 @@
 package com.example.cartpostservice.commissions.service.dto.request;
 
 import com.example.cartpostservice.common.model.vo.PaymentType;
-import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
-public record CommissionsSaveCommand(
+public record CommissionsServiceCommand(
         String memberCode,
 
         String title,
