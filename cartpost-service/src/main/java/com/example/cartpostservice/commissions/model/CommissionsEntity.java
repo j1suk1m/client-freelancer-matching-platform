95c0c27836b797c9d3c0a71df5670a1905a47ae4
sora0319 <sojeoung536985@gmail.com>
2025-11-18 06:41:21 +0900

    feat(post): 엔티티 nullable 속성 및 builder 추가

--- cartpost-service/src/main/java/com/example/cartpostservice/commissions/model/CommissionsEntity.java
+++ cartpost-service/src/main/java/com/example/cartpostservice/commissions/model/CommissionsEntity.java
@@ -2,10 +2,13 @@ package com.example.cartpostservice.commissions.model;
 
 import com.example.cartpostservice.common.model.vo.PaymentType;
 import com.example.cartpostservice.common.model.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 @Table(name = "commissions")
@@ -13,22 +16,42 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CommissionsEntity extends BaseEntity {
 
+    @Column(nullable = false)
     private String memberCode;
 
+    @Column(nullable = false)
     private String title;
 
+    @Column(nullable = false)
     private String content;
 
+    @Column(nullable = false)
     private PaymentType paymentType;
 
+    @Column(nullable = false)
     private String unitAmount;
 
+    @Column(nullable = false)
     private LocalDate startedAt;
 
+    @Column(nullable = false)
     private LocalDate endedAt;
 
-    private boolean isOpen;
+    @Column(nullable = false)
+    private boolean isOpen = true;
 
+    @Column(nullable = false)
     private String writerName;
 
+    @Builder
+    public CommissionsEntity(String memberCode, String title, String content, PaymentType paymentType, String unitAmount, LocalDate startedAt, LocalDate endedAt, String writerName){
+        this.memberCode = memberCode;
+        this.title = title;
+        this.content = content;
+        this.paymentType = paymentType;
+        this.unitAmount = unitAmount;
+        this.startedAt = startedAt;
+        this.endedAt = endedAt;
+        this.writerName = writerName;
+    }
 }
