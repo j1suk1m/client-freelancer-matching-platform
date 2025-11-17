162516672c44850825d92d2a74df1f92cda56486
sora0319 <sojeoung536985@gmail.com>
2025-11-18 06:39:00 +0900

    feat(post): 공통 서비스 CRUD 관리를 위한 인터페이스 생성

--- cartpost-service/src/main/java/com/example/cartpostservice/commissions/service/CrudService.java
+++ cartpost-service/src/main/java/com/example/cartpostservice/commissions/service/CrudService.java
@@ -6,9 +6,13 @@ import org.springframework.stereotype.Service;
 @Service
 public interface CrudService<T, code> {
 
-    public void save(T requestDto);
+    public String create(T requestDto);
 
     public Optional<T> read(code code);
 
+    public void update(T requestDto, code code);
+
     public void delete(code code);
+
+    public boolean exist(code ownerCode, code targetCode);
 }
