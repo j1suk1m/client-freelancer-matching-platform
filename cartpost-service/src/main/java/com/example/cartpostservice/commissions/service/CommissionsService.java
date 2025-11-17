b8b28fb514faa95b2131d929972895dfc3c91ece
sora0319 <sojeoung536985@gmail.com>
2025-11-18 06:46:09 +0900

    feat(post): service CRUD method 생성 및 create method 구현

--- cartpost-service/src/main/java/com/example/cartpostservice/commissions/service/CommissionsService.java
+++ cartpost-service/src/main/java/com/example/cartpostservice/commissions/service/CommissionsService.java
@@ -1,23 +1,52 @@
 package com.example.cartpostservice.commissions.service;
 
+import com.example.cartpostservice.commissions.model.CommissionsEntity;
+import com.example.cartpostservice.commissions.repository.CommissionsRepository;
+import com.example.cartpostservice.commissions.service.dto.request.CommissionsSaveCommand;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CommissionsService implements CrudService {
+@RequiredArgsConstructor
+public class CommissionsService implements CrudService<CommissionsSaveCommand, String> {
+
+    private final CommissionsRepository commissionsRepository;
 
     @Override
-    public void save(Object requestDto) {
+    public String create(CommissionsSaveCommand requestDto) {
+        CommissionsEntity commissions = CommissionsEntity.builder()
+                .memberCode(requestDto.memberCode())
+                .title(requestDto.title())
+                .content(requestDto.content())
+                .paymentType(requestDto.paymentType())
+                .unitAmount(requestDto.unitAmount())
+                .startedAt(requestDto.startedAt())
+                .endedAt(requestDto.endedAt())
+                .build();
 
+        CommissionsEntity saved = commissionsRepository.save(commissions);
+
+        return saved.getCode();
     }
 
     @Override
-    public Optional read(Object o) {
+    public Optional<CommissionsSaveCommand> read(String commissionsCode) {
         return Optional.empty();
     }
 
     @Override
-    public void delete(Object o) {
+    public void update(CommissionsSaveCommand requestDto, String commissionsCode) {
 
+    }
+
+    @Override
+    public void delete(String commissionsCode) {
+
+    }
+
+    @Override
+    public boolean exist(String ownerCode, String targetCode) {
+        return false;
     }
 }
