a4bfb1893d8a19dbd632f2732f3ba64435d215f5
ZZAMBAs <rksidksrksi@naver.com>
2025-11-19 01:45:00 +0900

    feat(contract): 테스트 코드 작성

--- contract-service/src/test/java/com/example/contractservice/deposit/service/DepositServiceTest.java
+++ contract-service/src/test/java/com/example/contractservice/deposit/service/DepositServiceTest.java
@@ -4,10 +4,18 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.example.contractservice.deposit.controller.dto.request.DepositRechargeRequest;
+import com.example.contractservice.deposit.controller.dto.response.DepositHistoryCursorResponse;
 import com.example.contractservice.deposit.domain.exception.DepositErrorCode;
 import com.example.contractservice.deposit.domain.exception.DepositException;
 import com.example.contractservice.deposit.entity.DepositEntity;
+import com.example.contractservice.deposit.entity.DepositHistoryEntity;
+import com.example.contractservice.deposit.repository.DepositHistoryJpaRepository;
 import com.example.contractservice.deposit.repository.DepositJpaRepository;
+import com.example.contractservice.deposit.service.dto.request.DepositHistoryCursorRequest;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,14 +24,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DepositServiceTest {
+
+    private static final int PAGE_SIZE = 20;
     @Autowired
     DepositService depositService;
     @Autowired
     DepositJpaRepository depositRepository;
+    @Autowired
+    DepositHistoryJpaRepository depositHistoryJpaRepository;
+
+    Random random = new Random();
 
     @AfterEach
     void tearDown() {
         depositRepository.deleteAllInBatch();
+        depositHistoryJpaRepository.deleteAllInBatch();
     }
 
     @Test
@@ -72,5 +87,42 @@ class DepositServiceTest {
         assertDoesNotThrow(() -> depositRepository.findByMemberCode(memberCode));
         DepositEntity entity = depositRepository.findByMemberCode(memberCode).get();
         assertEquals(memberCode, entity.getMemberCode().strip());
+    }
+
+    @Test
+    @DisplayName("자신의 예치금 변경 내역을 최신순으로 조회할 수 있다")
+    void success_get_latest_histories_given_normal() {
+        // given
+        String memberCode = UUID.randomUUID().toString();
+
+        DepositEntity depositEntity = depositRepository.save(DepositEntity.createBy(memberCode));
+
+        List<DepositHistoryEntity> entities = IntStream.range(0, 40)
+                .mapToObj(i -> {
+                    try {
+                        Thread.sleep(50L);
+                    } catch (Exception e) { }
+                    return DepositHistoryEntity.builder()
+                        .code(UUID.randomUUID().toString())
+                        .depositCode(depositEntity.getCode())
+                        .changeAmount(random.nextLong(2_000_000L))
+                        .resultAmount(random.nextLong(2_000_000L))
+                        .summary("" + i)
+                        .build();
+                }).toList();
+        depositHistoryJpaRepository.saveAll(entities);
+
+        // when
+        DepositHistoryCursorResponse firstResp = depositService.getDepositHistories(
+                new DepositHistoryCursorRequest(memberCode, null, null));
+        DepositHistoryCursorResponse secondResp = depositService.getDepositHistories(
+                new DepositHistoryCursorRequest(memberCode, firstResp.CursorDate(), firstResp.cursorCode()));
+
+        // then
+        assertEquals(PAGE_SIZE, firstResp.infos().size());
+        assertTrue(firstResp.hasNext());
+        assertEquals(PAGE_SIZE, secondResp.infos().size());
+        assertFalse(secondResp.hasNext());
+        assertTrue(firstResp.infos().get(0).createdAt().isAfter(secondResp.infos().get(0).createdAt()));
     }
 }
