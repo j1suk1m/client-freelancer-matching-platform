b983698859ffb499bbabf821c152fb59897b05b8
jisukimtech <jisukim.tech@gmail.com>
2025-11-19 23:09:38 +0900

    docs(swagger): 채팅방 생성 API 문서화 수정
    - 불필요한 애노테이션 삭제
    - 설정 파일 추가

--- communication-service/src/main/java/com/example/communicationservice/controller/api/ChatRoomControllerApi.java
+++ communication-service/src/main/java/com/example/communicationservice/controller/api/ChatRoomControllerApi.java
@@ -5,38 +5,39 @@ import com.example.communicationservice.controller.dto.request.ChatRoomCreateReq
 import com.example.communicationservice.controller.dto.response.ChatRoomCreateResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 
 @Tag(name = "ChatRoom API", description = "채팅방 CRUD")
 public interface ChatRoomControllerApi {
 
     @Operation(
         summary = "채팅방 생성",
-        description = "새로운 채팅방을 생성합니다.",
-        responses = {
-            @ApiResponse(
-                responseCode = "201",
-                description = "채팅방 생성 성공",
-                content = @Content(schema = @Schema(implementation = ChatRoomCreateResponse.class))
-            ),
-            @ApiResponse(responseCode = "400", description = "잘못된 요청")
-        }
+        description = "채팅방 이름과 참여할 회원 코드 목록으로 새로운 채팅방을 생성합니다."
     )
-    @PostMapping
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "채팅방 생성 성공"),
+        @ApiResponse(responseCode = "400", description = "잘못된 요청")
+    })
     ResponseEntity<ResponseDto<ChatRoomCreateResponse>> createRoom(
-        @Parameter(description = "채팅방 생성 요청 DTO", required = true)
-        @Valid @RequestBody ChatRoomCreateRequest request,
+        @RequestBody(
+            description = "채팅방 생성 요청 DTO",
+            required = true
+        )
+        ChatRoomCreateRequest request,
 
-        @Parameter(hidden = true)
-        @RequestHeader(name = "X-CODE", required = false) String currentMemberCode
+        @Parameter(
+            name = "X-CODE",
+            description = "현재 로그인한 회원 코드",
+            required = true,
+            in = ParameterIn.HEADER,
+            example = "abc-12345-ABC"
+        )
+        String currentMemberCode
     );
 
 }
